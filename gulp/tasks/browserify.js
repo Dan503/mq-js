645834f85a27d35fc2b4b5c3826a47740f8e75cc
Daniel Tonon <Dan503@users.noreply.github.com>
2017-02-25 23:36:03 +1100

    Reverting back to old browserify task
    I just realised that I don't want the index.js file to be browserified. I only want it to be babelified.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -14,65 +14,43 @@ import notifier from 'node-notifier';
 
 import { jsWatch, notification_icon_location} from '../config/shared-vars';
 
-let browserify_launch_pad = [];
 
 export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   let dirs = config.directories;
   let entries = config.entries;
 
-  function generate_browserify_tasks(done){
-    glob('./' + path.join(dirs.source, dirs.scripts, entries.js), function(err, files) {
-      if (err) {
-        console.error(err);
+  let browserifyTask = (done, files) => {
+    files.map((entry) => {
+      let dest = path.resolve(taskTarget, dirs.assets);
+
+      // Options
+      let customOpts = {
+        entries: [entry],
+        debug: true,
+        paths: [
+          './src/_modules',
+          './src/_scripts'
+        ],
+        transform: [
+          babelify, // Enable ES6 features
+          envify // Sets NODE_ENV for better optimization of npm packages
+        ]
+      };
+
+      let bundler = browserify(customOpts);
+
+      if (jsWatch.isEnabled) {
+        // Setup Watchify for faster builds
+        let opts = _.assign({}, watchify.args, customOpts);
+        bundler = watchify(browserify(opts));
+
       }
 
-      return browserifyTasks(done, files);
-    });
-  }
+      const jsErrorNotification = {title:'JS Error', message: 'Error running JS browserify'};
 
-  function browserifyTasks(done, files) {
-    files.map((entry) => {
-
-      const taskName = `browserify ${entry}`;
-
-      browserify_launch_pad.push(taskName);
-
-      gulp.task(taskName, (done)=>{
-        let dest = path.resolve(taskTarget, dirs.assets);
-
-        // Options
-        let customOpts = {
-          entries: [entry],
-          debug: true,
-          paths: [
-            './src/_modules',
-            './src/_scripts'
-          ],
-          transform: [
-            babelify, // Enable ES6 features
-            envify // Sets NODE_ENV for better optimization of npm packages
-          ]
-        };
-
-        let bundler = browserify(customOpts);
-
-        if (jsWatch.isEnabled) {
-          // Setup Watchify for faster builds
-          let opts = _.assign({}, watchify.args, customOpts);
-          bundler = watchify(browserify(opts));
-
-        }
-
-        const jsErrorNotification = {title:'JS Error', message: 'Error running JS browserify'};
-
+      let rebundle = function() {
         let startTime = new Date().getTime();
-
-        // if (!args.production) {
-        //   bundler.on('update', gulp.series(taskName)); // on any dep update, runs the bundler
-        //   bundler.on('log', plugins.util.log); // output build logs to terminal
-        // }
-
-        return bundler.bundle()
+        bundler.bundle()
           .on('error', function(err) {
             notifier.notify({title: 'JS Error', message: err.loc ? `${path.basename(error.filename)} line ${error.loc.line}` : 'JS failed to compile', icon: notification_icon_location+'gulp-error.png'});
             plugins.util.log(
@@ -101,40 +79,28 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
               plugins.util.colors.cyan(entry)
               + ' was browserified: '
               + plugins.util.colors.magenta(time + 's'));
-            //done();
+            browserSync.reload('*.js');
+
+            done();
           });
-      });
+      };
+
+      if (!args.production) {
+        bundler.on('update', rebundle); // on any dep update, runs the bundler
+        bundler.on('log', plugins.util.log); // output build logs to terminal
+      }
+      return rebundle();
     });
-
-    done();
-  };
-
-  function copy_index_to_root(done){
-    if (args.production){
-      return gulp.src('./' + path.join(taskTarget, dirs.assets, dirs.scripts.replace(/^_/,''), 'index.js'))
-        .pipe(plugins.rename({dirname: ''}))
-        .pipe(gulp.dest('./'));
-    } else {
-      done();
-    }
-  }
-
-  function rebundle_js (done) {
-    return gulp.parallel(...browserify_launch_pad)(done);
   };
 
   // Browserify Task
-  gulp.task('browserify', gulp.series(
-    (done)=>{
-      browserify_launch_pad = [];
-      done();
-    },
-    generate_browserify_tasks,
-    rebundle_js,
-    (done)=>{
-      browserSync.reload('*.js');
-      done();
-    },
-    copy_index_to_root
-  ));
+  gulp.task('browserify', (done) => {
+    return glob('./' + path.join(dirs.source, dirs.scripts, entries.js), function(err, files) {
+      if (err) {
+        done(err);
+      }
+
+      return browserifyTask(done, files);
+    });
+  });
 }
