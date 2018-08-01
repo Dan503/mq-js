5a970c238688682620c048adc50a8a4785aa98e0
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-08-01 23:11:05 +1000

    Added more reactTo tests

--- tests/reactTo.js
+++ tests/reactTo.js
@@ -5,27 +5,30 @@ import mq from './_helpers/mq';
 import report_result_summary from './_helpers/report_result_summary';
 import sequence from './_helpers/sequence';
 import ResultTracker from './_helpers/ResultTracker';
+import { screenSize } from '../_common';
 
 import resize from './_helpers/windowResize';
 
 export default function(){
 
-	let reactValue = false;
+	let reactValue = { isActive: false, isEqual: false };
 	let hasReacted = false;
 	let allowReactions = false;
 
-	mq.reactTo(()=> mq.max(bp.large), (isActive)=> {
+	mq.reactTo(()=> mq.max(bp.large), (isActive, screen_size)=> {
 		if (allowReactions) {
-			console.log({isActive});
 			hasReacted = true;
-			reactValue = isActive
+			reactValue = {
+				isActive,
+				isEqual: ()=> isActive === mq.max(bp.large),
+				screen_size: ()=> JSON.stringify(screenSize()) === JSON.stringify(screen_size)
+			}
 		}
-	})
+	});
 
 	const initialState = ()=> new Promise((resolve)=> {
 		resize(bp.large + 1);
 		allowReactions = true;
-		console.log('initialised');
 		resolve();
 	});
 
@@ -34,10 +37,10 @@ export default function(){
 	let reactResults = new ResultTracker();
 
 	class positiveTest extends Test {
-		constructor(){
+		constructor({name, test}){
 			super({
-				name:`positive reactTo`,
-				test: ()=> reactValue,
+				name: `positive reactTo ${name}`,
+				test,
 				size: [bp.large],
 				mqMatch: 'ignore',
 				localTracker: reactResults,
@@ -46,10 +49,10 @@ export default function(){
 	}
 
 	class negativeTest extends Test {
-		constructor(){
+		constructor({name, test}){
 			super({
-				name:`negative reactTo`,
-				test: ()=> !reactValue && hasReacted,
+				name:`negative reactTo ${name}`,
+				test,
 				size: [bp.large+1],
 				mqMatch: 'ignore',
 				localTracker: reactResults,
@@ -57,11 +60,48 @@ export default function(){
 		}
 	}
 
+	const positiveTests = [
+		new positiveTest({
+			name:`activation`,
+			test: ()=> reactValue.isActive,
+		}),
+		new positiveTest({
+			name:`match`,
+			test: ()=> reactValue.isEqual(),
+		}),
+		new positiveTest({
+			name:`screen-size`,
+			test: ()=> reactValue.screen_size(),
+		}),
+		new positiveTest({
+			name:`screen-size format regex`,
+			test: ()=> {
+				const size = screenSize();
+				const string = JSON.stringify(size);
+				const regex = /{"width":([0-9]*),"height":([0-9]*),"orientation":"(portrait|landscape)","ratio":{"number":([0-9]),"string":"([0-9]) \/ ([0-9])"}}/;
+				return regex.test(string);
+			},
+		}),
+	];
+
+	const negativeTests = [
+		new negativeTest({
+			name:`activation`,
+			test: ()=> !reactValue.isActive && hasReacted,
+		}),
+		new negativeTest({
+			name:`match`,
+			test: ()=> reactValue.isEqual() && hasReacted,
+		}),
+	];
+
+	console.log(reactValue);
+
 	return sequence([
 		initialState,
 		delay,
-		new positiveTest(),
-		new negativeTest(),
+		...positiveTests,
+		...negativeTests,
 		report_result_summary('reactTo', reactResults),
 		()=> Promise.resolve(()=> allowReactions = false),
 	])
