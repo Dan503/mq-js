698c4f030a0091cc498d20ef2a62ed29d5c17268
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-08-18 16:24:33 +1000

    some .btn to .a-btn conversions I missed

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -43,7 +43,7 @@ _$('#testingBtn').click(window.test);
 const toggleActive = elem => elem.classList.toggle('-active');
 const toggleInactive = elem => elem.classList.toggle('-inactive');
 
-_$('.btn.-max').onclick = function(e){
+_$('.a-btn.-max').onclick = function(e){
   e.preventDefault();
 
   mq.max('medium', (screen_size)=>{
@@ -52,17 +52,17 @@ _$('.btn.-max').onclick = function(e){
   })
 };
 
-toggler('.btn.-maxIf', ()=> mq.max('medium'));
-toggler('.btn.-min', ()=> mq.min('medium'));
-toggler('.btn.-inside', ()=> mq.inside('medium', 'small'));
-toggler('.btn.-outside', ()=> mq.outside('medium', 'small'));
+toggler('.a-btn.-maxIf', ()=> mq.max('medium'));
+toggler('.a-btn.-min', ()=> mq.min('medium'));
+toggler('.a-btn.-inside', ()=> mq.inside('medium', 'small'));
+toggler('.a-btn.-outside', ()=> mq.outside('medium', 'small'));
 
 const MQ_btn = state => ({
   active: () => mq.max('medium'),
   inactive: () => mq.min('medium'),
 }[state]());
 
-_$('.btn.-mqVar').onclick = function(e){
+_$('.a-btn.-mqVar').onclick = function(e){
   e.preventDefault();
 
   if (MQ_btn('active')){
@@ -75,11 +75,11 @@ _$('.btn.-mqVar').onclick = function(e){
 
 };
 
-toggler('.btn.-pxVal', ()=> mq.max(1000));
-toggler('.btn.-pxValMin', ()=> mq.min(1000));
-toggler('.btn.-bpVar', ()=> mq.min(bp.medium + 30));
+toggler('.a-btn.-pxVal', ()=> mq.max(1000));
+toggler('.a-btn.-pxValMin', ()=> mq.min(1000));
+toggler('.a-btn.-bpVar', ()=> mq.min(bp.medium + 30));
 
-const _$resizeBtn = _$('.btn.-onResize');
+const _$resizeBtn = _$('.a-btn.-onResize');
 
 _$resizeBtn.onclick = (e) => e.preventDefault();
 
@@ -90,20 +90,20 @@ function toggleBtn(){
 toggleBtn();
 window.onresize = debounce(toggleBtn, 200);
 
-toggler('.btn.-maxHeight', ()=> mq.maxHeight(600));
-toggler('.btn.-minHeight', ()=> mq.minHeight(600));
-toggler('.btn.-insideHeight', ()=> mq.insideHeight(800, 400));
-toggler('.btn.-outsideHeight', ()=> mq.outsideHeight(800, 400));
-toggler('.btn.-landscape', ()=> mq.orientation('landscape'));
-toggler('.btn.-portrait', ()=> mq.orientation('portrait'));
-toggler('.btn.-exactRatio', ()=> mq.ratio(1 / 2));
-toggler('.btn.-minRatio', ()=> mq.ratio('1 / 2'));
-toggler('.btn.-maxRatio', ()=> mq.ratio(1 / 2));
-toggler('.btn.-insideRatio', ()=> mq.insideRatio(1 / 2, 3 / 2));
-toggler('.btn.-insideRatio', ()=> mq.outsideRatio(1 / 2, 3 / 2));
-toggler('.btn.-maxRatioString', ()=> mq.maxRatio('1 / 2'));
+toggler('.a-btn.-maxHeight', ()=> mq.maxHeight(600));
+toggler('.a-btn.-minHeight', ()=> mq.minHeight(600));
+toggler('.a-btn.-insideHeight', ()=> mq.insideHeight(800, 400));
+toggler('.a-btn.-outsideHeight', ()=> mq.outsideHeight(800, 400));
+toggler('.a-btn.-landscape', ()=> mq.orientation('landscape'));
+toggler('.a-btn.-portrait', ()=> mq.orientation('portrait'));
+toggler('.a-btn.-exactRatio', ()=> mq.ratio(1 / 2));
+toggler('.a-btn.-minRatio', ()=> mq.ratio('1 / 2'));
+toggler('.a-btn.-maxRatio', ()=> mq.ratio(1 / 2));
+toggler('.a-btn.-insideRatio', ()=> mq.insideRatio(1 / 2, 3 / 2));
+toggler('.a-btn.-insideRatio', ()=> mq.outsideRatio(1 / 2, 3 / 2));
+toggler('.a-btn.-maxRatioString', ()=> mq.maxRatio('1 / 2'));
 
-const _$btn = _$('.btn.-reactTo');
+const _$btn = _$('.a-btn.-reactTo');
 
 mq.reactTo(()=> mq.inside(800, 900), (is_active, screen_size)=> {
   _$btn.classList.toggle('-active');
