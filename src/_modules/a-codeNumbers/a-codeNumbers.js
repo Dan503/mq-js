7c097d7d915f72b69696bd5935cbe899f69a923c
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-08-22 23:47:07 +1000

    Refactored codeNumbers to avoid array mutation

--- src/_modules/a-codeNumbers/a-codeNumbers.js
+++ src/_modules/a-codeNumbers/a-codeNumbers.js
@@ -6,34 +6,33 @@ export default function(){
 	// putting lines by the pre blocks
 	_$$("pre:not(.no-lines)").forEach(function(_$code, index, listObj){
 		const lines = _$code.innerText.split("\n");
-
 		const numbers = lines.map((text, i) => i+1);
-
-		const remove_empty_ends = (numbersArr, linesArr) => {
-			trim_numbers(numbersArr, linesArr);
-			const reversed = [...linesArr].reverse();
-			trim_numbers(numbersArr, reversed);
-		}
-
-		function trim_numbers(numbersArr, linesArr) {
-			linesArr.some(line => {
-				if (line === '') {
-					numbersArr.pop();
-					return false;
-				}
-				return true;
-			})
-		}
-
-		remove_empty_ends(numbers, lines);
+		const trimmed_numbers = remove_empty_ends(numbers, lines);
 
 		const originalHTML = _$code.outerHTML;
-
 		_$code.outerHTML = `
-		<div class="a-codeNumbers">
-			<pre class="a-codeNumbers__numbers">${numbers.join("\n")}</pre>
-			<div class="a-codeNumbers__code">${originalHTML}</div>
-		</div>`;
+			<div class="a-codeNumbers">
+				<pre class="a-codeNumbers__numbers">${trimmed_numbers.join("\n")}</pre>
+				<div class="a-codeNumbers__code">${originalHTML}</div>
+			</div>`;
 	});
+
+	function remove_empty_ends (numbersArr, linesArr) {
+		let clonedNumbers = [...numbersArr];
+		trim_numbers(clonedNumbers, linesArr);
+		const reversed = [...linesArr].reverse();
+		trim_numbers(clonedNumbers, reversed);
+		return clonedNumbers;
+	}
+
+	function trim_numbers(numbersArr, linesArr) {
+		linesArr.some(line => {
+			if (line === '') {
+				numbersArr.pop();
+				return false;
+			}
+			return true;
+		})
+	}
 
 }
