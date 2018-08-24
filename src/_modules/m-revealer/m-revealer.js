0a80498f201b11fa6f6bd826abbc66cab47a740b
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-08-24 22:28:08 +1000

    Opens relevant revealer if # is detected

--- src/_modules/m-revealer/m-revealer.js
+++ src/_modules/m-revealer/m-revealer.js
@@ -1,19 +1,27 @@
 
-const m = '.m-revealer';
+import _$$ from '../../_scripts/_helpers/_$selector';
+
+const m = 'm-revealer';
+const _m = '.'+m;
 
 class Revealer {
 	constructor(wrapperElem){
 		this._$wrapper = wrapperElem;
-		this._$trigger = this._$wrapper.querySelector(`${m}__trigger`);
-		this._$slider = this._$wrapper.querySelector(`${m}__slider`);
-		this._$content = this._$wrapper.querySelector(`${m}__content`);
+		this._$trigger = this._$wrapper.querySelector(`${_m}__trigger`);
+		this._$slider = this._$wrapper.querySelector(`${_m}__slider`);
+		this._$content = this._$wrapper.querySelector(`${_m}__content`);
 		this.isReady = false;
 		this.timer;
+		this.relevantIDs = this.gather_ids();
+		console.log(this.relevantIDs);
 		this.initialise();
 	}
 
 	initialise(){
-		this.close();
+		this.isOpen = this.check_url();
+		if (!this.isOpen) {
+			this.close();
+		}
 		this._$wrapper.classList.add('-set');
 		this._$trigger.onclick = ()=> this.toggle();
 		this.isReady = true;
@@ -66,9 +74,40 @@ class Revealer {
 			return Promise.resolve(()=> this.isReady = true);
 		});
 	}
+
+	check_url(){
+		const hash = window.location.hash.replace('#','');
+		const hasID = this.relevantIDs.some(id => id === hash);
+		console.log({hasID})
+		return hasID;
+	}
+
+	gather_ids(){
+		const children = _$$(_m, this._$content);
+		const child_trigger_ids = children.map(get_id);
+
+		return [this._$trigger.id, ...child_trigger_ids];
+
+		function get_id (_$wrapper) {
+			const _$trigger = _$wrapper.querySelector(`${_m}__trigger`);
+			return _$trigger.id;
+		}
+	}
+
+	get_parents_revelers(){
+		let parents = [];
+		let elem = this._$trigger;
+		while (elem.classList) {
+			if (elem.classList.contains(m)){
+				parents.push(elem);
+			}
+			elem = elem.parentNode;
+		}
+		return parents;
+	}
 }
 
 export default function(){
-	const _$$revealers = [...document.querySelectorAll(m)];
+	const _$$revealers = [...document.querySelectorAll(_m)];
 	_$$revealers.forEach(_$revealer => new Revealer(_$revealer));
 }
--- src/_scripts/_helpers/_$selector.js
+++ src/_scripts/_helpers/_$selector.js
@@ -1,7 +1,7 @@
 
 const isString = string => typeof string === 'string';
-const _$ = selector => isString(selector) ? document.querySelector(selector) : selector;
-const _$$ = selector => isString(selector) ? [...document.querySelectorAll(selector)] : selector;
+const _$ = (selector, _$refElem = document) => isString(selector) ? _$refElem.querySelector(selector) : selector;
+const _$$ = (selector, _$refElem = document) => isString(selector) ? [..._$refElem.querySelectorAll(selector)] : selector;
 
 export default _$$;
 export { _$$, _$ }
