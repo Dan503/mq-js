20a62bf719c1f804989279adf092bab17f4648a2
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-08-24 21:36:27 +1000

    Fixed height not updating when content height changes

--- src/_modules/m-revealer/m-revealer.js
+++ src/_modules/m-revealer/m-revealer.js
@@ -7,24 +7,32 @@ class Revealer {
 		this._$trigger = this._$wrapper.querySelector(`${m}__trigger`);
 		this._$slider = this._$wrapper.querySelector(`${m}__slider`);
 		this._$content = this._$wrapper.querySelector(`${m}__content`);
+		this.isReady = false;
+		this.timer;
 		this.initialise();
 	}
 
 	initialise(){
 		this.close();
-		this.set_max_height();
 		this._$wrapper.classList.add('-set');
 		this._$trigger.onclick = ()=> this.toggle();
+		this.isReady = true;
 	}
 
 	open(){
 		this._$wrapper.classList.remove('-closed');
-		this.isOpen = true;
+		this.wait_for_animation().then(()=>{
+			this.isOpen = true;
+			this.clear_max_height();
+		});
 	}
 
 	close() {
-		this._$wrapper.classList.add('-closed');
-		this.isOpen = false;
+		this.set_max_height();
+		this.delay(1).then(()=>{
+			this._$wrapper.classList.add('-closed');
+			this.isOpen = false;
+		})
 	}
 
 	toggle(){
@@ -38,6 +46,25 @@ class Revealer {
 	set_max_height(){
 		const height = getComputedStyle(this._$content).height;
 		this._$slider.style.maxHeight = height;
+	}
+	clear_max_height(){
+		this._$slider.style.maxHeight = 'none';
+	}
+
+	delay(delay){
+		return new Promise(resolve => {
+			setTimeout(resolve, delay);
+		})
+	}
+
+	wait_for_animation(){
+		clearTimeout(this.timer);
+		this.isReady = false;
+		return new Promise(resolve => {
+			this.timer = setTimeout(resolve, 500);
+		}).then(()=> {
+			return Promise.resolve(()=> this.isReady = true);
+		});
 	}
 }
 
